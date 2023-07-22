98cf5d58f111ab9c1743228db70db7bd1ed71435
Daniel Augusto Mandira <105872910+DanielMandira@users.noreply.github.com>
2023-07-22 20:28:50 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,87 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator, StackView } from '@react-navigation/stack';
+import Home from './src/home/index';
+import Login from './src/login';
+import Cadastro from './src/cadastro/index';
+import Caduser from './src/cadastro/cadastroUser';
+import CadEmp from './src/cadastro/cadastroEmpresa';
+import BemVindo from './src/login/boasVindas';
+const Stack = createStackNavigator();
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login'>
+        <Stack.Screen
+          name='Login'
+          component={Login}
+          options={
+            {
+              headerTransparent: true,
+              title: '',
+              headerShown: false,
+              presentation:'transparentModal'
+            }
+          } />
+          <Stack.Screen
+          name='Home'
+          component={Home}
+          options={
+            {
+              headerTransparent: true,
+              title: '',
+              headerShown: false,
+              presentation:'transparentModal'
+            }
+          } />
+          <Stack.Screen
+          name='Cadastro'
+          component={Cadastro}
+          options={
+            {
+              headerTransparent: true,
+              title: '',
+              headerShown: false,
+              presentation:'transparentModal',
+            }
+          } />
+          <Stack.Screen 
+          name='Caduser'
+          component={Caduser}
+          options={
+            {
+              headerTransparent: true,
+              title: '',
+              headerShown: true,
+              presentation:'transparentModal',
+            }
+          }
+          />
+           <Stack.Screen 
+          name='CadEmp'
+          component={CadEmp}
+          options={
+            {
+              headerTransparent: true,
+              title: '',
+              headerShown: true,
+              presentation:'transparentModal',
+            }
+          }
+          />
+           <Stack.Screen 
+          name='BemVindo'
+          component={BemVindo}
+          options={
+            {
+              headerTransparent: true,
+              title: '',
+              headerShown: false,
+              presentation:'transparentModal',
+            }
+          }
+          />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
